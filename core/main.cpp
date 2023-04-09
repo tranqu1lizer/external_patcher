3495c8925434984fce57f840e43242c50044432a
F0RQU1N <108815933+F0RQU1N@users.noreply.github.com>
2023-04-09 22:47:20 +1000

    Update main.cpp

--- core/main.cpp
+++ core/main.cpp
@@ -1,6 +1,4 @@
-﻿#include "dota_sdk.hpp"
-
-// bool showParticles = false;
+#include "dota_sdk.hpp"
 
 int main( )
 {
@@ -14,12 +12,6 @@ int main( )
 			std::cout << "Attached to dota2.exe " << std::endl;
 			std::vector<blackbone::ptr_t> search_result;
 			blackbone::PatternSearch aDOTACameraInit_Pattern{ "\x48\x83\xEC\x38\xE8\xCC\xCC\xCC\xCC\x48\x85\xC0\x74\x4D" };
-			//blackbone::PatternSearch aDOTAParticleManager_Pattern{ xorstr_( "\xE8\xCC\xCC\xCC\xCC\x8B\x14\x9F" ) };
-
-			//aDOTAParticleManager_Pattern.SearchRemote( dota, 0xCC, pClientModule.get( )->baseAddress, pClientModule.get( )->size, search_result, 1 );
-			//search_result[0] = search_result[0] + ( pDOTAMemory.Read<int>( search_result[0] + 1 ).result( ) + 5 );
-			//const auto pDOTAParticleManager = search_result[0] + ( pDOTAMemory.Read<int>( search_result[0] + 3 ).result( ) + 7 );
-			//search_result.clear( );
 
 			while ( 1 ) {
 				std::cout << "PID: " << std::dec << dota.pid( ) << std::endl << "client.dll base: " << (void*)pClientModule.get( )->baseAddress << std::endl;
@@ -48,24 +40,6 @@ int main( )
 						else if ( act == 2 ) {
 							DOTACamera.ToggleFog( );
 						}
-						/*else if ( act == 3 ) {
-							showParticles = !showParticles;
-							auto addr = pDOTAMemory.Read<std::uintptr_t>( pDOTAParticleManager ).result( );
-							CDOTA_ParticleManager ParticleManager( &pDOTAMemory, addr );
-
-							std::cout << addr << std::endl;;
-							auto particlesList = ParticleManager.GetParticleLists( );
-							for ( auto& particle : particlesList ) {
-								if ( auto particleCollection = particle.GetNewParticleEffect( ).GetParticleCollection( ); particleCollection.baseAddr ) {
-									blackbone::RemoteFunction<SetRenderingEnabled> pFNRendering( dota, particle.GetNewParticleEffect( ).GetParticleCollection( ).GetVF( 95 ) );
-									pFNRendering.Call( particleCollection.baseAddr, true );
-								}
-							}
-
-						}
-						else if ( act == 4 ) {
-							exit( 0 );
-						}*/
 						else exit( 0 );
 					}
 					else {
